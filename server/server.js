e3b3c0d0e731de0383648607bebe497657a99f46
Enel <spiritring@gmail.com>
2013-02-27 22:27:00 +0800

    更新游戏目录树
    更新游戏目录树

--- server/server.js
+++ server/server.js
@@ -1,19 +1,3 @@
-/************************************************************************
- *  Copyright (c) 2011-2012 SHA Junxing.
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- ***********************************************************************/
-
 "use strict";
 
 var log = require('./log');
@@ -25,14 +9,10 @@ log.setLevel(config.logLevel);
 log.setColoredOutput(config.coloredOutput);
 
 console.info();
-console.info('    ----   ------------');
-console.info('     ||   | --      -- |');
-console.info('     ||   |     --     |');
-console.info('    --     ------------');
+console.info('    TimeShiftGameServer!');
+console.info('    Leader: Enel-Pu');V
+console.info('    ersion 1.0');
 console.info();
-console.info('    Json Messaging version 1.0');
-console.info();
-
 
 tcp.createServer(config.tcpPort, config.maxFrameSize);
 ws.createServer(config.wsPort, config.maxFrameSize);
